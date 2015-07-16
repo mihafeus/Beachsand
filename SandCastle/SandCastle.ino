13929bd830b76232f690651f0e196e908ecf46f1
Miha Feus <mihafeus@uk-m-mfeus.ideo.com>
2015-07-16 17:34:56 +0100

    Improved SandCastle code
    Now you can plug in the receiver directly to the Arduino. Just don’t
    forget to cut the CS pin on the receiver

--- SandCastle/SandCastle.ino
+++ SandCastle/SandCastle.ino
@@ -2,22 +2,22 @@
 //http://forum.arduino.cc/index.php?topic=167953.0 //some receiver code by jmc
 //http://stackoverflow.com/questions/25989202/arduino-virtualwire-separating-received-data // some receiver code by Wasd9595
 
-
 #include <VirtualWire.h>
 
-int ledPin = 13; // LED
 int Sensor1Data; // Sensors 
 char Sensor1CharMsg[4]; // RF Transmission container
 
-int LBit = 5;
+int LBit = 5; //for littleBits integration
+
+int powerPin = 11; //to give power to the receiver if you decide to plug it streight into the Arduino
 
 void setup() {
   vw_set_ptt_inverted(true); // Required for DR3100
-
-  vw_set_rx_pin(12); // Connect this pin to the DATA pin on your receiver
+  vw_set_rx_pin(13); // Connect this pin to the DATA pin on your receiver
 
   vw_setup(1000);  // Bits per second - the speed of the communication 
-  pinMode(ledPin, OUTPUT); // LED
+  pinMode(powerPin, OUTPUT);
+  digitalWrite(powerPin, HIGH); //power on
   Serial.begin(9600); // set baud rate of serial communication 
   vw_rx_start();       // Start the receiver PLL running    
 
@@ -32,8 +32,6 @@ void loop(){
   uint8_t buf[buflen]; // The buffer that holds the message
 
   if (vw_get_message(buf, &buflen)){   
-
-    digitalWrite(13, true); //if a message is received the LED will blink
 
     buf[buflen] = '\0'; //Null terminate the char array, otherwhise problems could occur with values of different lenghts
     String str((char *)buf); //Converts received values to a string
@@ -51,12 +49,9 @@ void loop(){
     Serial.print("  Value: ");
     Serial.println(Sensor1Data);
 
-
     int sensScale = Sensor1Data/4; //for the littleBits integration, converts the analogIn value for analogWrite
     analogWrite(LBit, sensScale); //littleBits integration, this passes through a low-pass filter on the ciruit 0.1uF, 4.7KOhm
 
-    digitalWrite(13, false); // Turn off light to and await next message 
-
-
   } // END if vw_get_message
 } // END loop
+
